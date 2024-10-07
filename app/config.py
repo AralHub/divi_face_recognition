83f17911df9c0d82d7f7c422dad3f6204db21deb
Babur Parakhatov <melomanovmeloman@gmail.com>
2024-10-07 18:56:22 +0500

    first commit

--- app/config.py
+++ app/config.py
@@ -1,7 +1,11 @@
+import os
+
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+load_dotenv()
 
 class Settings(BaseSettings):
-    mongodb_url: str = "mongodb://localhost:27017"
+    mongodb_url: str = os.getenv('MONGO_URL')
     #mongodb_url: str = "mongodb://mongo:27017/face_recognition"
     database_name: str = "face_recognition"
     collections: str = ("collections")
