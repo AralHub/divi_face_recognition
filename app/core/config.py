116e6d446c3726e0f95ccc943a846970f9cf9157
Babur Parakhatov <melomanovmeloman@gmail.com>
2025-05-16 12:48:17 +0500

    Add .env.example file and update config.py for MongoDB settings

--- app/core/config.py
+++ app/core/config.py
@@ -6,30 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-class S3Config(BaseSettings):
-    aws_access_key_id: str = os.environ.get("AWS_ACCESS_KEY_ID")
-    aws_secret_access_key: str = os.environ.get("AWS_SECRET_ACCESS_")
-    endpoint_url: str = os.environ.get("ENDPOINT_URL")
-    bucket_name: str = os.environ.get("BUCKET_NAME")
-    server_name: str = os.environ.get("SERVER_NAME")
-
-
-class Redis(BaseSettings):
-    host: str = os.environ.get("REDIS_HOST")
-    port: int = os.environ.get("REDIS_PORT")
-    password: str = os.environ.get("REDIS_PASS")
-
 
 class Settings(BaseSettings):
     MONGODB_URL: str = os.environ.get("MONGODB_URL")
     DATABASE_NAME: str = os.environ.get("DATABASE_NAME")
-    COLLECTIONS: str = os.environ.get("COLLECTIONS")
-    MODEL_NAME: str = "buffalo_l"
-    MODEL_PATH: str = os.environ.get("MODEL_PATH")
     WORKER_POOL_SIZE: int = 1
-    s3_config: S3Config = S3Config()  # Указание типа атрибута.
-    redis_config: Redis = Redis()  # Указание типа атрибута.
-    
+
     # Новые настройки для 3DiVi
     DIVI_SDK_PATH: str = os.environ.get("DIVI_SDK_PATH", "/home/stargroup/3DiVi_FaceSDK/3_25_1/")
     USE_CUDA: bool = os.environ.get("USE_CUDA", "False").lower() == "asd"
